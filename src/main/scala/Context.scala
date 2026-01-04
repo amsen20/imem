7423c81c8a5dc00eee5bb05f0d01560537b2a1f1
amsen <ahph1380@gmail.com>
2026-01-04 15:17:13 -0500

    Mention why context does not capture the lifetimes in a type parameter

--- src/main/scala/Context.scala
+++ src/main/scala/Context.scala
@@ -2,6 +2,13 @@ package imem
 
 import language.experimental.captureChecking
 
+/*
+  * TODO: Check if capturing the aggregated owners would not cause a problem.
+  * The other option is to aggregate them through a type parameter in the Context class.
+  * But the other option didn't work with Scinear due to mentioning lifetimes in nested scopes.
+  * The other option is preferable because it does not allow the user to change the owners due to
+  * covariance or contravariant.
+  */
 class Context[WC^, MC^]
 
 def withOwnership[T](block: [@caps.use WC^, MC^] => Context[WC, MC]^ => T): T =
