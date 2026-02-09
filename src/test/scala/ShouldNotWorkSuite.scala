bc3d3ff212bb338bb2d1fe43924af91a87c764c6
amsen <ahph1380@gmail.com>
2026-02-08 23:15:14 -0500

    Add should work and should not work tests for iterators

--- src/test/scala/ShouldNotWorkSuite.scala
+++ src/test/scala/ShouldNotWorkSuite.scala
@@ -1,177 +1,51 @@
 import language.experimental.captureChecking
+import imem.*
+import scinear.utils.LinearInt
 
-class ResourceShouldNotWorkSuite extends munit.FunSuite {
-  test("should not be able to call the Box default constructor") {
-    imem.withOwnership: ctx =>
-      // TODO: Should not compile, should make the constructor private.
-      // FIXME: Due to explained reason, it will evaluate fine.
-      intercept[Exception] {
-        new imem.Box()
-      }
-  }
+// omitting any of `O1`, or `O2` would result in a compile error:
+//                                                 v   v
+def longest[O4^, O1^ >: {O4}, O2^ >: {O4}, O3^ >: {O1, O2}, WC^, MC^](
+  a: ImmutRef[String, O1],
+  b: ImmutRef[String, O2]
+)(using ctx: Context[WC, MC]^{O4}): ImmutRef[String, O3] =
+  val isALessEqualB = read[String, O1, Boolean, O4, WC, MC](a, aVal =>
+    read[String, O2, Boolean, O1, WC, MC](b, bVal =>
+      aVal.length <= bVal.length
+    )
+  )
+  if isALessEqualB then
+    // if `O1` is omitted here, it would result in a compile error:
+    borrowImmut[String, O1, O4, NeverUsableKey, O3, WC, MC](a)
+    //                                          ^
+    // Type argument O3 does not conform to lower bound scala.caps.CapSet^{O1}
+  else
+    // if `O2` is omitted here, it would result in a compile error:
+    borrowImmut[String, O2, O4, NeverUsableKey, O3, WC, MC](b)
+    //                                          ^
+    // Type argument O3 does not conform to lower bound scala.caps.CapSet^{O2}
 
-  test("box should not be able assigned to a variable") {
-    imem.withOwnership: ctx =>
-      // TODO: A `Box` type should not be able to be assigned to a variable because it forces the user to use `Box`'s
-      // methods to set and reset it. Using them we can keep track of the ownership state.
-      // FIXME: Due to explained reason, it will evaluate fine.
-      intercept[Exception] {
-        var myVal = imem.newBoxExplicit[Int, {}](42)
-      }
-  }
+def IteratorsOnList[O1^, O2^ >: {O1}, @caps.use WC^, @caps.use MC^]()(using ctx: Context[WC, MC]^{O2}): Unit =
+  // create an empty list with {ctx} as its lifetime
+  val list = newBoxFromBackground(newLinkedListFromBackground[LinearInt, {WC}, {MC}])
 
-  test("should not be able to write to a reference, by reading it") {
-    imem.withOwnership: ctx =>
-      // A way to express a mutable integer.
-      case class BoxedInteger(var value: Int)
-      val myVal = imem.newBoxFromBackground(BoxedInteger(42))(using ctx)
+  // a lifetime for the mutable iterator
+  val lf1 = Lifetime[{ctx}]()
 
-      // TODO: For now, there is no difference between, `read` and `write` methods. Both can mutate the value. This
-      // Should be changed, at least in runtime or compile-time, mutation through `read`s should not be allowed.
-      // FIXME: Due to explained reason, it will evaluate fine.
-      intercept[IllegalStateException] {
-        imem.readBox[BoxedInteger, {ctx}, Unit, {ctx}](myVal, v => v.value = 12)(using ctx)
-      }
-  }
+  // borrow the list mutably to use it for the mutable iterator
+  val (listRef, listHolder) = imem.borrowMutBox[List[LinearInt, {ctx}], {ctx}, {ctx}, lf1.Key, lf1.Owners, {WC}, {MC}](list)
+  // create the mutable iterator
+  val mutIter = iterMut[LinearInt, {ctx}, lf1.Owners, lf1.Owners, {WC}, {MC}](listRef)
+  // don't need `iter` anymore, so consume it
+  mutIter.consume()
 
-  test("should not be able to escape a value through `read`/`write` methods of a reference") {
-    imem.withOwnership: ctx =>
-      // A way to express a mutable integer.
-      case class BoxedInteger(var value: Int)
-      val myVal = imem.newBoxFromBackground(BoxedInteger(42))(using ctx)
+  // get the list back
+  val list2 = unlockHolder(lf1.getKey(), listHolder)
 
-      intercept[Exception] {
-        /*
-        * TODO: The feature is implemented, but it emits a compile error. Should find a way to
-        * test compile errors (something like the compiler plugin tests).
-        * For now a dummy exception is thrown to show that this test fails (as expected).
-        */
-        // imem.readBox[BoxedInteger, {ctx}, BoxedInteger, {ctx}](myVal, v => v)(using ctx).value
-        throw new Exception("")
-      }
-  }
+  // create a consuming iterator from the list
+  val consumingIter = intoIter[LinearInt, {ctx}, {ctx}, {WC}, {MC}](list2)
+  // wrap it in a box
+  val iterBox = newBox[ConsumingIterator[LinearInt, {ctx}], {ctx}](consumingIter)
+  // don't need `iterBox` anymore, so consume it
+  iterBox.consume()
 
-  test("should not be able to mutate, while reading it") {
-    imem.withOwnership: ctx =>
-      // A way to express a mutable integer.
-      case class BoxedInteger(var value: Int)
-      val myVal = imem.newBoxFromBackground(BoxedInteger(42))(using ctx)
-
-      val immutRef = imem.borrowImmutBox[BoxedInteger, {ctx}, {ctx}, {ctx}](myVal)(using ctx)
-      imem.writeBox[BoxedInteger, {ctx}, Unit, {ctx}](myVal, _.value = 12)(using ctx)
-      intercept[IllegalStateException] {
-        imem.read[BoxedInteger, {ctx}, Unit, {ctx}](immutRef, _ => ())(using ctx)
-      }
-  }
-
-  test("borrows should be invalidated after moving") {
-    imem.withOwnership: ctx =>
-      // A way to express a mutable integer.
-      case class BoxedInteger(var value: Int)
-      val myVal = imem.newBoxFromBackground(BoxedInteger(42))(using ctx)
-      val immutRef = imem.borrowImmutBox[BoxedInteger, {ctx}, {ctx}, {ctx}](myVal)(using ctx)
-
-      // TODO: For now no moving runtime/compile time effect is defined, so the following line will have no effect in the
-      // stacked borrows. Should track moves in at least runtime or compile time.
-      val myOtherVal = myVal
-
-      // FIXME: Due to explained reason, it will evaluate fine.
-      intercept[IllegalStateException] {
-        imem.read[BoxedInteger, {ctx}, Unit, {ctx}](immutRef, _ => ())(using ctx)
-      }
-  }
-
-  test(
-    "should not be able to go around a reference connection with its owner by wrapping it up in another reference"
-  ) {
-    imem.withOwnership: ctx =>
-      val main = imem.newBoxFromBackground(imem.newBoxFromBackground(1)(using ctx))(using ctx)
-      val dummy = imem.newBoxFromBackground(2)(using ctx)
-
-      val ref1 = imem.borrowMutBox[imem.Box[Int, {ctx}], {ctx}, {ctx}, {ctx}](main)(using ctx)
-      val dummyRef = imem.borrowImmutBox[Int, {ctx}, {ctx}, {ctx}](dummy)(using ctx)
-
-      val ref2 = imem.write[imem.Box[Int, {ctx}], {ctx}, imem.ImmutRef[Int, {ctx}], {ctx}](
-        ref1,
-        (inner: imem.Box[Int, {ctx}]^) =>
-          imem.read[Int, {ctx}, imem.ImmutRef[Int, {ctx}], {ctx}](
-            dummyRef,
-            _ => imem.borrowImmutBox[Int, {ctx}, {ctx}, {ctx}](inner)(using ctx)
-          )
-      )(using ctx)
-
-      imem.write[imem.Box[Int, {ctx}], {ctx}, Unit, {ctx}](ref1, _ => ())(using ctx) // should expire `ref2`
-      intercept[IllegalStateException] {
-        // imem.read(ref2, _ => ())(using ctx)
-      }
-      imem.read(dummyRef, _ => ())(using ctx)
-  }
-
-  test(
-    "should invalidate every reference from a box after the box is out of scope"
-  ) {
-    imem.withOwnership: ctx =>
-      // FIXME: should not be able to compile this
-      // TODO: For now, I don't know how to define it, so I comment it.
-      // def leakImmutRef(b: imem.Box[Int]^) =
-      // b.borrowImmut
-      // NOTE: For now, intentionally fail this test.
-      assert(false)
-  }
-
-  test("should invalidate a box owned by another box after the box is out of scope") {
-    imem.withOwnership: ctx =>
-      // FIXME: should not be able to compile this
-      // TODO: For now, I don't know how to define it, so I comment it.
-      // def leakMutRef(outer: imem.Box[imem.Box[Int]]^) =
-      // outer.borrowImmut.read(identity)
-      // NOTE: For now, intentionally fail this test.
-      assert(false)
-  }
-
-  test("should not be able to access self owned boxes in any way other than their holders") {
-    imem.withOwnership: ctx =>
-      val orig: imem.OwnerOrigin^ = new imem.OwnerOrigin
-      val orig2: imem.OwnerOrigin^ = new imem.OwnerOrigin
-      val listHolder = new imem.BoxHolder[orig.Key, imem.Box[Int, {orig}]^{orig}, {orig}](imem.newBoxExplicit[imem.Box[Int, {orig}]^{orig}, {orig}](imem.newBoxExplicit[Int, {orig}](42)))
-      // OK
-      imem.useBoxHolder(listHolder, orig.getKey(), list => ())(using ctx)
-
-      /*
-      * FIXME: For now compile errors, cannot be tested in this unit, can be fixed by writing
-      * tests like the compiler plugin tests.
-      */
-      // val list2 = listHolder.getBox(orig2.getKey())
-      // val list3 = listHolder.getBox(new Object)
-  }
-}
-
-class ListShouldNotWorkSuite extends munit.FunSuite {
-
-  test("should not be able to push while peeking immutably") {
-    imem.withOwnership: ctx =>
-      val list = imem.newBoxFromBackground(newLinkedListFromBackground[Int](using ctx))(using ctx)
-      push(imem.borrowMutBox(list)(using ctx), 1)(using ctx)
-
-      val res = peek[Int, {ctx}, {ctx}, {ctx}, {ctx}](imem.borrowImmutBox(list)(using ctx))(using ctx)
-
-      intercept[IllegalStateException] {
-        push(imem.borrowMutBox(list)(using ctx), 2)(using ctx)
-        imem.read(res.get, _ => ())(using ctx) // idle read
-      }
-      ()
-  }
-
-  test("should not be able to push while peeking mutably") {
-    imem.withOwnership: ctx =>
-      val list = imem.newBoxFromBackground(newLinkedListFromBackground[Int](using ctx))(using ctx)
-      push(imem.borrowMutBox(list)(using ctx), 1)(using ctx)
-
-      val res = peekMut[Int, {ctx}, {ctx}, {ctx}, {ctx}](imem.borrowMutBox(list)(using ctx))(using ctx)
-
-      intercept[IllegalStateException] {
-        push(imem.borrowMutBox(list)(using ctx), 2)(using ctx)
-        imem.write(res.get, _ => ())(using ctx) // idle read
-      }
-  }
-}
+  ()
--- src/test/scala/ShouldWorkSuite.scala
+++ src/test/scala/ShouldWorkSuite.scala
@@ -1,5 +1,6 @@
 import language.experimental.captureChecking
 import scinear.utils.LinearInt
+import imem.*
 
 class BoxedInteger[O^](_value: imem.Box[Int, O]) extends scinear.Linear:
   val value: imem.Box[Int, O]^{this} = _value
@@ -99,7 +100,7 @@ class ListShouldWorkSuite extends munit.FunSuite:
     imem.withImem([WC^, MC^] => ctx => body[WC, MC](using ctx))
   }
 
-  test("peek and peekMut") {
+  test("peek and peekMut and iterMut and iterConsume") {
     def body[@caps.use WC^, @caps.use MC^](using ctx: imem.Context[WC, MC]^): Unit =
       val list = imem.newBoxFromBackground(newLinkedListFromBackground[BoxedInteger[{ctx}], WC, MC](using ctx))(using ctx)
 
@@ -138,7 +139,6 @@ class ListShouldWorkSuite extends munit.FunSuite:
         val InnerLf = new imem.Lifetime[{ctx}]()
         val (listRef, listHolder) = imem.borrowMutBox[List[BoxedInteger[{ctx}], {ctx}], {ctx}, {ctx}, InnerLf.Key, InnerLf.Owners, {WC}, MC](list5)
         val peekedMut = peekMut[BoxedInteger[{ctx}], {ctx}, InnerLf.Owners, {ctx}, InnerLf.Key, InnerLf.Owners, {WC}, {MC}](listRef).get
-        // TODO: CHECK
         imem.write[BoxedInteger[{ctx}], InnerLf.Owners, Unit, {ctx}, {WC}, {MC}](peekedMut, (boxedInt: BoxedInteger[{ctx}]^) => imem.setBox[Int, {ctx}, {ctx, InnerLf}, {WC}, {MC}](boxedInt.value, 42))(using ctx)
         imem.unlockHolder(InnerLf.getKey(), listHolder)
 
@@ -152,7 +152,49 @@ class ListShouldWorkSuite extends munit.FunSuite:
 
         imem.unlockHolder(InnerLf.getKey(), listHolder)
 
-      list7.consume()
+      val list8 =
+        val InnerLf = new imem.Lifetime[{ctx}]()
+        val (listRef, listHolder) = imem.borrowMutBox[List[BoxedInteger[{ctx}], {ctx}], {ctx}, {ctx}, InnerLf.Key, InnerLf.Owners, {WC}, MC](list7)
+        val iter = iterMut[BoxedInteger[{ctx}], {ctx}, {ctx}, InnerLf.Owners, {WC}, {MC}](listRef)
+        val iterBox = imem.newBox[MutableIterator[BoxedInteger[{ctx}], {ctx}, InnerLf.Owners], InnerLf.Owners](iter)
+        val (iterRef, iterHolder) = imem.borrowMutBox[MutableIterator[BoxedInteger[{ctx}], {ctx}, InnerLf.Owners], InnerLf.Owners, {ctx}, InnerLf.Key, InnerLf.Owners, {WC}, {MC}](iterBox)
+        iterHolder.consume()
+        val firstElemOp2 = nextMI[BoxedInteger[{ctx}], {ctx}, InnerLf.Owners, InnerLf.Owners, {WC}, {MC}](iterRef)
+
+        val (firElemOpt2, isEmpty) = scinear.utils.peekLinearOption(firstElemOp2)
+        assert(!isEmpty)
+        val firstElem2 = firElemOpt2.get
+        val is42 = imem.write[BoxedInteger[{ctx}], InnerLf.Owners, Boolean, {ctx}, {WC}, {MC}](firstElem2, (boxedInt: BoxedInteger[{ctx}]^) => imem.readBox[Int, {ctx}, Boolean, {ctx, InnerLf}, {WC}, {MC}](boxedInt.value, _ == 42)._2)
+        assert(is42)
+
+        imem.unlockHolder(InnerLf.getKey(), listHolder)
+
+      val iter = intoIter[BoxedInteger[{ctx}], {ctx}, {ctx}, {WC}, {MC}](list8)
+      val iterBox = imem.newBox[ConsumingIterator[BoxedInteger[{ctx}], {ctx}], {ctx}](iter)
+
+      val iterBox2 =
+        val InnerLf = new imem.Lifetime[{ctx}]()
+        val (iterRef, iterHolder) = imem.borrowImmutBox[ConsumingIterator[BoxedInteger[{ctx}], {ctx}], {ctx}, {ctx}, InnerLf.Key, InnerLf.Owners, {WC}, {MC}](iterBox)
+        val hasNext = hasNextCI[BoxedInteger[{ctx}], {ctx}, InnerLf.Owners, {WC}, {MC}](iterRef)
+        assert(hasNext)
+        imem.unlockHolder(InnerLf.getKey(), iterHolder)
+
+      val iterBox3 =
+        val InnerLf = new imem.Lifetime[{ctx}]()
+        val (iterRef, iterHolder) = imem.borrowMutBox[ConsumingIterator[BoxedInteger[{ctx}], {ctx}], {ctx}, {ctx}, InnerLf.Key, InnerLf.Owners, {WC}, {MC}](iterBox2)
+        val firstElemOpt = nextCI[BoxedInteger[{ctx}], {ctx}, InnerLf.Owners, {WC}, {MC}](iterRef)
+
+        val (firstElemOpt2, isEmpty) = scinear.utils.peekLinearOption(firstElemOpt)
+        assert(!isEmpty)
+        val firstElem = firstElemOpt2.get
+        val (firstElemImmutRef, firstElemImmutHolder) = borrowImmutBox[BoxedInteger[{ctx}], InnerLf.Owners, {ctx}, imem.NeverUsableKey, InnerLf.Owners, {WC}, {MC}](firstElem)
+        firstElemImmutHolder.consume()
+        val is42 = imem.read[BoxedInteger[{ctx}], InnerLf.Owners, Boolean, {ctx}, {WC}, {MC}](firstElemImmutRef, (boxedInt: BoxedInteger[{ctx}]^) => imem.readBox[Int, {ctx}, Boolean, {ctx, InnerLf}, {WC}, {MC}](boxedInt.value, _ == 42)._2)
+        assert(is42)
+
+        imem.unlockHolder(InnerLf.getKey(), iterHolder)
+
+      iterBox3.consume()
       ()
     imem.withImem([WC^, MC^] => ctx => body[WC, MC](using ctx))
   }
